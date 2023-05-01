d6972b019c26903257055e67f221cea5eae87ab0
Vatsal Adhiya <vatsaladhiya248@gmail.com>
2023-05-01 10:16:00 +0530

    Solved one math problem

---
+++ LeetCode/Easy/ConvertTheTemperature.java
@@ -0,0 +1,11 @@
+package LeetCode.Easy;
+// https://leetcode.com/problems/convert-the-temperature/
+
+class Solution {
+    public double[] convertTemperature(double celsius) {
+        double[] d = new double[2];
+        d[0] = celsius + 273.15;
+        d[1] = (celsius * 1.80) + 32.00;
+        return d;
+    }
+}
